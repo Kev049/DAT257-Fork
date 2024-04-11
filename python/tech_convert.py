fa16aad81f783fecc84a08017e607f095d70684d
Kevin Collins <nivek049@gmail.com>
2024-04-11 16:53:11 +0200

    Worked more on creating a datasheet with multiple types of renewable energy
    Co-authored-by: Heldereim <Heldereim@users.noreply.github.com>

--- python/tech_convert.py
+++ python/tech_convert.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 
-stats = pd.read_csv('python/ELECSTAT-C_20240411-135404.csv')
+stats = pd.read_csv('python/ELECSTAT-C_20240411-142227.csv')
 stats = stats[['Country/area', 'Electricity statistics']]
 stats.columns = ['Country', input("Give the type of energy: ") + " (GWh)"]
 
-master_doc = pd.read_csv('resources/Renewable_data.csv')
+master_doc = pd.read_csv('output.csv')
 
 final_df = master_doc.merge(stats, how='left', on='Country')
 final_df.to_csv('output.csv',index=None)
+
