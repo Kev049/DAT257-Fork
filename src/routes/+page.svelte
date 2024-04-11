e2be981fcd7c27d37e53523a0ca137a09dcaf743
Simon Hammerlid <simon.hammerlid@gmail.com>
2024-04-11 14:26:04 +0200

    changed stuf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,19 @@
             Logo
         </div>
         <div class="flex justify-center items-center m-0 text-white text-4xl font-dosis">
-            <input type="text" placeholder=" Search..." class="rounded-lg text-black font-dosis width: 80%" name="searchfield">
-            <button type="submit" class="background: #2196F3 float: left width: 20% border: 1px solid grey"><i class="searchbutton"></i></button>
+            <form class="w-full">
+                <!-- <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label> -->
+                <div class="flex absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                </div>
+                <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 font-dosis" placeholder="Search Mockups, Logos..." required />
+                <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-dosis rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+            <!-- <input type="text" placeholder=" Search..." class="rounded-lg text-black font-dosis width: 80%" name="searchfield">
+            <button type="submit" class="background: #2196F3 float: left width: 20% border: 1px solid grey"><i class="searchbutton"></i></button> -->
             <!-- Search -->
+            </form>
         </div>
         <div class="flex justify-end pr-16 items-center m-0 font-dosis text-white text-2xl">
             Menu
