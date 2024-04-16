70a16e9e9a40e8c2f7b438a508b32afd10e9c3eb
Simon Hammerlid <simon.hammerlid@gmail.com>
2024-04-16 21:20:22 +0200

    Html table now shown when going to country site

--- src/routes/[country]/+page.server.js
+++ src/routes/[country]/+page.server.js
@@ -1,18 +1,14 @@
 /** @type {import('./$types').PageServerLoad} */
 export async function load({params}) {
 
-    const response = await fetch(`http://127.0.0.1:5000/${params.country}`)//.then((response) => response.json()).then((json) => console.log(json));
-    const data = await response.json()
-    console.log(data)
+    const response = await fetch(`http://127.0.0.1:5000/${params.country}`);//.then((response) => response.json()).then((json) => console.log(json));
+    const data = await response.text();
+    //console.log(data)
     const country_data = data[0]
-
+    //console.log(country_data)
+    
 
     return {
-        country: country_data['Country'],
-        code: country_data['Code (alpha-3)'],
-        renewablePercent: country_data['Renewable energy production (%)'],
-        SolarPhotoVolt: country_data['Solar Photovoltaic (GWh)'],
-        SolarThermal: country_data['Solar Thermal (GWh)']
-
+        data
     };
 };
--- src/routes/[country]/+page.svelte
+++ src/routes/[country]/+page.svelte
@@ -1,8 +1,8 @@
 <script>
-    /** @type {import('./$types').PageData} */
     export let data;
+    //console.log(data);
 </script>
 
-<h1>{data.country}</h1>
-<p>{data.code}</p>
-<p>{data.renewablePercent}</p>
+<div>
+    {@html data.data}
+</div>
