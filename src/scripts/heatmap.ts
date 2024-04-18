55fab299db518a299ae8872b42acfdea1fc43735
Liam Mayor <liamn.mayor@gmail.com>
2024-04-18 11:53:08 +0200

    Added comments

--- src/scripts/heatmap.ts
+++ src/scripts/heatmap.ts
@@ -18,15 +18,15 @@ interface SVGCoordinate {
     y: number;
 }
 
-proj4.defs("ROBINSON", "+proj=robin +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs");
-
-const wgs84 = 'EPSG:4326';
+// Using proj4js to project geographical coordinates using Robinson projection. 
+// There's a lot of hard coding as the SVG we use does not include Antarctica and is a variation of a normal Robinson projection. 
+const wgs84 = 'EPSG:4326'; // Output
 const robinson = '+proj=robin +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs';  // Robinson projection string
 
 function convertCoordinates(lat: number, lon: number, svgDimensions: SVGDimensions): SVGCoordinate {
     const { width, height } = svgDimensions;
 
-    lon -= 2.5;
+    lon -= 2.5; // There is a slight offset in the map, might be bad to hard code the solution this way.
 
     const minY = proj4(wgs84, robinson, [0, -60])[1];
     const maxY = proj4(wgs84, robinson, [0, 85])[1];
@@ -37,6 +37,7 @@ function convertCoordinates(lat: number, lon: number, svgDimensions: SVGDimensio
     const minX = proj4(wgs84, robinson, [-180, 0])[0];
     const maxX = proj4(wgs84, robinson, [180, 0])[0];
 
+    // Adapting to the SVG
     const effectiveWidth = maxX - minX;
     const effectiveHeight = maxY - minY;
     const xScale = width / effectiveWidth;
@@ -48,6 +49,8 @@ function convertCoordinates(lat: number, lon: number, svgDimensions: SVGDimensio
     return { x, y };
 }
 
+
+// I imagine we will adjust the functions below, this is just temp code.
 function calculateIntensity(solarEnergy: number): number {
     const maxEnergy = 3000;  // Max expected solar energy
     return Math.min(solarEnergy / maxEnergy, 1);
