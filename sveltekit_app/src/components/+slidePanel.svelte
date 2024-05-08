4d89ffd75fad041cd2a10e310735f9be9849b428
Simon Hammerlid <simon.hammerlid@gmail.com>
2024-05-08 11:49:21 +0200

    Update +slidePanel.svelte

--- sveltekit_app/src/components/+slidePanel.svelte
+++ sveltekit_app/src/components/+slidePanel.svelte
@@ -28,5 +28,5 @@
     <!-- {#if showImage}
         {@html countryGraph}
     {/if} -->
-    <button></button>
+    <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"></button>
 </div>
